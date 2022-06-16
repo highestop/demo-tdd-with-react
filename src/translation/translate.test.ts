6a1ad7083a4d562eef2cb967209830458cd40f9c
ChenYn <highestopchen@gmail.com>
2022-06-16 12:16:50 +0800

    del: remove translation

--- src/translation/translate.test.ts
+++
@@ -1,7 +0,0 @@
-function translate(c: string): any {
-  return c === "d" ? { Dev: 0.5 } : { Dev: 1.0 };
-}
-
-it("translates D to one dev day", () => {
-  expect(translate("D")).toEqual({ Dev: 1.0 });
-});
