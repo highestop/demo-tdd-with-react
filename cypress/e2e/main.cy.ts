32e4e788ffaa1db13f497dd65e9cd899e13303f2
ChenYn <highestopchen@gmail.com>
2022-06-16 15:34:40 +0800

    refaactor: use server post and delete

--- cypress/e2e/main.cy.ts
+++ cypress/e2e/main.cy.ts
@@ -1,4 +1,28 @@
+import axios from "axios";
+
 describe("react application", function () {
+  before(() => {
+    return axios
+      .delete("http://localhost:8119/books?_cleanup=true")
+      .catch((err) => err);
+  });
+  afterEach(() => {
+    return axios
+      .delete("http://localhost:8119/books?_cleanup=true")
+      .catch((err) => err);
+  });
+  beforeEach(() => {
+    const books = [
+      { name: "Refactoring", id: 1 },
+      { name: "Domain-driven design", id: 2 },
+    ];
+    return books.map((item) =>
+      axios.post("http://localhost:8119/books", item, {
+        headers: { "Content-Type": "application/json" },
+      })
+    );
+  });
+
   it("Visits the bookish", function () {
     cy.visit("https://localhost:8118/");
     cy.get('div[data-test="book-list"]').should("exist");
