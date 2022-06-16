07703f2a514f4428ffa58b92e43115d08abb2a48
ChenYn <highestopchen@gmail.com>
2022-06-16 11:49:07 +0800

    fix: ts-jest

--- jest.config.ts
+++ jest.config.ts
@@ -2,8 +2,6 @@
  * For a detailed explanation regarding each configuration property and type check, visit:
  * https://jestjs.io/docs/configuration
  */
-import { RawCompilerOptions } from "ts-jest";
-import tsConfig from "./tsconfig.json";
 
 export default {
   // All imported modules in your tests should be mocked automatically
@@ -67,11 +65,7 @@ export default {
   // globalTeardown: undefined,
 
   // A set of global variables that need to be available in all test environments
-  globals: {
-    "ts-jest": {
-      tsconfig: tsConfig.compilerOptions as RawCompilerOptions,
-    },
-  },
+  // globals: {},
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
   // maxWorkers: "50%",
