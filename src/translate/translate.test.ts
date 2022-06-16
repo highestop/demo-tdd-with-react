0825529ebb836b2bed02bc477889b58d4923a100
ChenYn <highestopchen@gmail.com>
2022-06-16 12:19:06 +0800

    feat: add test 2

--- src/translate/translate.test.ts
+++ src/translate/translate.test.ts
@@ -5,3 +5,7 @@ function translate(c: string) {
 it("translates d to half a dev day", () => {
   expect(translate("d")).toEqual({ Dev: 0.5 });
 });
+
+it("translates D to one dev day", () => {
+  expect(translate("D")).toEqual({ Dev: 1.0 });
+});
