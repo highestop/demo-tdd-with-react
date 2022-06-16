32f4eebcebc4c8782dcb2e027b4d9f39a5c3f5a7
ChenYn <highestopchen@gmail.com>
2022-06-16 18:19:41 +0800

    refactor: rename root to app

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   // our 'index.html' is in src folder, not default project root folder. Noted that environment meta providers are also in root folder.
-  root: "src/react",
+  root: "src/app",
   // build outdir is same as root by default, but we want to keep it in root project root folder, so this is relative to root.
   build: {
     outDir: "../../dist",
